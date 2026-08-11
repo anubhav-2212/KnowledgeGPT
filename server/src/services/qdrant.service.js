a58f8808780ea9956b0b0ce76273d572b35c6f19
anubhav-2212 <srivastvaanubhav.2212@gmail.com>
2026-08-11 10:06:36 +0530

    feat: update qdrant service to support multi-tenancy via knowledgeBaseId environment variable and remove test script

--- server/src/services/qdrant.service.js
+++ server/src/services/qdrant.service.js
@@ -1,6 +1,7 @@
 import {qdrant} from "../utils/qdrant.js";
+import "dotenv/config";
 
-const COLLECTION_NAME = "knowledge-base";
+const COLLECTION_NAME = process.env.QDRANT_COLLECTION_NAME;
 
 export const ensureCollection = async (vectorSize) => {
     const exists = await qdrant.collectionExists(COLLECTION_NAME);
@@ -26,11 +27,21 @@ export const upsertVectors = async (vectors) => {
     });
 };
 
-export const searchVectors = async (queryVector, limit = 5) => {
+export const searchVectors = async (queryVector, knowledgeBaseId,limit = 5) => {
     const res=await qdrant.query(COLLECTION_NAME, {
         query: queryVector,
         limit,
         with_payload: true,
+        filter: {
+            must: [
+                {
+                    key: "knowledgeBaseId",
+                    match: {
+                        value: knowledgeBaseId,
+                    },
+                },
+            ],
+        },
     });
     return res.points;
 };
